29bec5d5a5af0a8db0823548c6c22c2302aedfc2
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-07-16 08:53:57 +0530

    first complete

--- src/components/LayoutFirst.jsx
+++ src/components/LayoutFirst.jsx
@@ -17,16 +17,26 @@ const LayoutFirst = () => {
 
     
   };
+
+  const breakpoints = {
+    sm: '2em',
+    md: '2.5em',
+    lg: '3.5em',
+    xl: '5em',
+    '2xl': '6em',
+  }
   return (
     <Box p="0.5rem" pt="4rem" height="90vh">
       <SimpleGrid columns={[1, 3]} width="100%" height="100%">
-        <Box bg="#0072BB" {...TextStyles}>
+        <Box bg="#0072BB" {...TextStyles} fontSize={breakpoints}>
           NAV
         </Box>
-        <Box bg="#F8931D" {...TextStyles}>
+        <Box bg="#F8931D" {...TextStyles} 
+        fontSize={breakpoints}>
           CONTENT
         </Box>
-        <Box bg="red" {...TextStyles}>
+        <Box bg="red" {...TextStyles}
+        fontSize={breakpoints}>
           WIDGET
         </Box>
       </SimpleGrid>
